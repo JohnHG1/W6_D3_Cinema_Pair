f3cd61ecaeaa3f2d1980d2594bdd407c98bb9314
JohnHG1 <honisz@yahoo.com>
2020-02-19 15:28:17 +0000

    completed first four MVP

--- models/cinema.js
+++ models/cinema.js
@@ -4,6 +4,27 @@ const Cinema = function (name, films =[]){
   this.films = films;
 }
 
+  Cinema.prototype.addFilm = function(film){
+    this.films.push(film);
+  };
+
+  Cinema.prototype.getTitles = function(){
+   return this.films.map((film) => {
+     return film.title;
+   });
+  };
+
+  Cinema.prototype.getFilmByTitle = function(title){
+    return this.films.find((film) => {
+      return (film.title === title )
+    })
+  }
+
+  Cinema.prototype.getFilmByGenre = function(genre){
+    return this.films.filter((film) => {
+      return (film.genre === genre )
+    })
+  }
 
 
 
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -14,12 +14,39 @@ describe('Cinema', function() {
     film1 = new Film("Jaws", "horror", 1977, 120);
     film2 = new Film("Jaws2", "horror", 1980, 150);
     film3 = new Film("Babe", "kids", 2000, 120);
-
+    cinema.addFilm(film1);
+    cinema.addFilm(film2);
+    cinema.addFilm(film3);
   });
 
   it('should have a name', function(){
     const actual=cinema.name;
     assert.strictEqual(actual, 'The cinema');
+  })
+
+  it('should have films', function(){
+    const actual=cinema.films
+    assert.deepStrictEqual(actual, [film1,film2,film3])
+  })
+
+  it('should be able ot get all film titles', function(){
+    const actual = cinema.getTitles();
+    assert.deepStrictEqual(actual, ["Jaws", "Jaws2", "Babe"]);
+  })
+
+  it('should be able to find a film by title', function(){
+    const actual =cinema.getFilmByTitle('Jaws');
+    assert.deepStrictEqual(actual, film1)
+  })
+
+  it('should be able to find a film by genre', function(){
+    const actual =cinema.getFilmByGenre('horror');
+    assert.deepStrictEqual(actual, [film1, film2])
+  })
+
+  it('should be able to find films by year', function(){
+
+    
   })
 
 });
